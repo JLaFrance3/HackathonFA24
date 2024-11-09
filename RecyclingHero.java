9be62eec3d386c047e51de31fa38b67ea707997e
jlafrance3 <jlafranceiii@proton.me>
2024-11-09 10:59:04 -0500

    Throwables

--- RecyclingHero.java
+++ RecyclingHero.java
@@ -22,33 +22,16 @@ public class RecyclingHero extends JFrame {
         binsPanel.add(new Bin(BinType.METAL, new Point(400,0)));
         binsPanel.add(new Bin(BinType.PAPER, new Point(50, 300)));
         binsPanel.add(new Bin(BinType.TRASH, new Point(500, 200)));
-        // binsPanel.add(createDraggableItem(BinType.GLASS));
-        // binsPanel.add(createDraggableItem(BinType.PLASTIC));
-        // binsPanel.add(createDraggableItem(BinType.METAL));
-        // binsPanel.add(createDraggableItem(BinType.PAPER));
-        // binsPanel.add(createDraggableItem(BinType.TRASH));
+
+        // Add draggable items to binsPanel
+        binsPanel.add(new ThrowableItem(BinType.GLASS, new Point(0, 400)));
+        binsPanel.add(new ThrowableItem(BinType.PLASTIC, new Point(100, 400)));
+        binsPanel.add(new ThrowableItem(BinType.METAL, new Point(200, 400)));
+        binsPanel.add(new ThrowableItem(BinType.PAPER, new Point(300, 400)));
+        binsPanel.add(new ThrowableItem(BinType.TRASH, new Point(400, 400)));
 
         add(binsPanel);
     }
-
-    // private JLabel createDraggableItem(BinType type) {
-    //     Bin item = new Bin(type);
-    //     item.setTransferHandler(new TransferHandler("text"));
-    //     item.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-    //     item.setPreferredSize(new Dimension(100, 100));
-    //     item.setOpaque(true);
-    //     item.setBackground(Color.LIGHT_GRAY);
-
-    //     item.addMouseListener(new java.awt.event.MouseAdapter() {
-    //         public void mousePressed(java.awt.event.MouseEvent evt) {
-    //             JComponent comp = (JComponent) evt.getSource();
-    //             TransferHandler handler = comp.getTransferHandler();
-    //             handler.exportAsDrag(comp, evt, TransferHandler.COPY);
-    //         }
-    //     });
-
-    //     return item;
-    // }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
---
+++ ThrowableItem.java
@@ -0,0 +1,56 @@
+
+/*
+* Class to represent a recycling or trash bin
+*/
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.TransferHandler;
+
+public class ThrowableItem extends JLabel{
+    private BinType type;
+    private Point location;
+    private BufferedImage binImage;
+
+    public ThrowableItem(BinType type) {
+        super(type.name());
+        this.type = type;
+        this.location = new Point(0, 0);
+        setTransferHandler(new TransferHandler("text"));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setSize(new Dimension(100, 100));
+        setOpaque(true);
+        setBackground(Color.GRAY);
+
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                JComponent comp = (JComponent) evt.getSource();
+                TransferHandler handler = comp.getTransferHandler();
+                handler.exportAsDrag(comp, evt, TransferHandler.COPY);
+            }
+        });
+    }
+
+    public ThrowableItem(BinType type, Point location) {
+        this(type);
+        setLocation(location);
+    }
+
+    public ThrowableItem(BinType type, Point location, BufferedImage binImage) {
+        this(type, location);
+        this.binImage = binImage;
+    }
+
+    public Point getLocation() {return location;}
+    public void setLocation(Point location) {this.location = location;}
+    public int getX() {return location.x;}
+    public int getY() {return location.y;}
+    public BinType getType() {return type;}
+    public void setType(BinType type) {this.type = type;}
+}
