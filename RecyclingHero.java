c6d5967381f86ef08de352fd7403b9974cc40e9a
dkasy64 <dawitkasy64@gmail.com>
2024-11-09 12:07:04 -0500

    saving changes

--- RecyclingHero.java
+++ RecyclingHero.java
@@ -8,12 +8,18 @@ import java.io.IOException;
 public class RecyclingHero extends JFrame {
     BufferedImage glassImage = null;
     BufferedImage plasticImage = null;
+    BufferedImage metalImage = null;
+    BufferedImage paperImage = null;
+    BufferedImage trashImage = null;
+    BufferedImage grassImage = null;
+    
+
     
     private ScoreBoard scoreBoard;
 
     public RecyclingHero() {
         setTitle("Recycling Hero");
-        setSize(800, 600);
+        setSize(1600, 1600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
@@ -25,19 +31,24 @@ public class RecyclingHero extends JFrame {
         try {
             glassImage = ImageIO.read(new File("Resources\\glassBin.png"));
             plasticImage = ImageIO.read(new File("Resources\\plasticBin.png"));
+            metalImage = ImageIO.read(new File("Resources\\metalBin.png"));
+            paperImage = ImageIO.read(new File("Resources\\goodPaperBin.png"));
+            trashImage = ImageIO.read(new File("Resources\\trash bin.png"));
+            grassImage = ImageIO.read(new File("Resources\\coolGrass.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         // Create bins panel and position it below the scoreboard
-        JPanel binsPanel = new JPanel();
+        ImagePanel binsPanel = new ImagePanel(grassImage);
         binsPanel.setLayout(null); // set icon and set border to null
-        binsPanel.setBounds(0, 0, 800, 600);
+        binsPanel.setBounds(0, 0, 1600, 1600);
+        
         binsPanel.add(new Bin(BinType.GLASS, new Point(200, 50), glassImage));
-        binsPanel.add(new Bin(BinType.PLASTIC, new Point(100, 10), plasticImage));
-        binsPanel.add(new Bin(BinType.METAL, new Point(400,0)));
-        binsPanel.add(new Bin(BinType.PAPER, new Point(50, 300)));
-        binsPanel.add(new Bin(BinType.TRASH, new Point(500, 200)));
+        binsPanel.add(new Bin(BinType.PLASTIC, new Point(100, 50), plasticImage));
+        binsPanel.add(new Bin(BinType.METAL, new Point(400,110), metalImage));
+        binsPanel.add(new Bin(BinType.PAPER, new Point(50, 300), paperImage));
+        binsPanel.add(new Bin(BinType.TRASH, new Point(500, 200), trashImage));
 
         // Add draggable items to binsPanel
         binsPanel.add(new ThrowableItem(BinType.GLASS, new Point(0, 400)));
